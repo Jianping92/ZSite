594b8b157df2048adc533490448f2fdf04937692
zhaojp <zhaojp@beidasoft.com>
2021-08-25 23:02:11 +0800

    增加dayjs

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    "@nuxtjs/dayjs"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
